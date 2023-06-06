8129cd2c410ee1d895fc463498b45ee3516f733a
harish242 <120125264+harish242@users.noreply.github.com>
2023-06-06 17:42:03 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,11 @@
+// import {state} from "../components/App"
 const counterReducer = (state,action) =>{
-
+if(action.type==="Increment"){
+    return {count:state.count+1}
+}
+if(action.type==="Decrement"){
+    return {count:state.count-1}
+}
 }
 
 export {counterReducer}
